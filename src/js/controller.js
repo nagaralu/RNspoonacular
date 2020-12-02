4d8053ac99f610504ab6f965e16abe19e5dc9f68
nagaralu <rnsm25@gmail.com>
2020-12-02 00:24:25 -0600

    recipe function added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,4 @@
+import { API_KEY } from './config.js';
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -9,3 +10,32 @@ const timeout = function (s) {
 };
 
 ///////////////////////////////////////
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      `https://api.spoonacular.com/recipes/214146/information?apiKey=${API_KEY}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let recipe = data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.sourceName,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image,
+      servings: recipe.servings,
+      cookingTime: recipe.readyInMinutes,
+      ingredients: recipe.extendedIngredients,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();
